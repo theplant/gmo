46dbceca4f52c33251d81fd25656f062bf7bb58d
bom_d_van <bom.d.van@gmail.com>
2015-06-25 11:18:48 +0800

    refactor tests

--- gmo_test.go
+++ gmo_test.go
@@ -19,85 +19,114 @@ func init() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 }
 
-func checkErr(err error, t *testing.T) error {
-	if err != nil {
-		t.Error(err.Error())
-	}
-	return err
-}
-
 func TestMemberAPIs(t *testing.T) {
 	userID := fmt.Sprintf("%v", time.Now().UnixNano())
-	if _, err := Client.RegisterMember(userID, "jinzhu"); checkErr(err, t) == nil {
-		if output, err := Client.SearchMember(userID); checkErr(err, t) == nil {
-			if output.MemberID != userID || output.MemberName != "jinzhu" {
-				t.Errorf("User should be registered successfully")
-			}
-		}
+	if _, err := Client.RegisterMember(userID, "Mr Tester"); err != nil {
+		t.Error(err)
+	} else if output, err := Client.SearchMember(userID); err != nil {
+		t.Error(err)
+	} else if output.MemberID != userID || output.MemberName != "Mr Tester" {
+		t.Errorf("User should be registered successfully")
 	}
 
-	if _, err := Client.UpdateMember(userID, "jinzhu-new"); checkErr(err, t) == nil {
-		if output, err := Client.SearchMember(userID); checkErr(err, t) == nil {
-			if output.MemberID != userID || output.MemberName != "jinzhu-new" {
-				t.Errorf("User should be updated successfully")
-			}
-		}
+	if _, err := Client.UpdateMember(userID, "Miss Tester"); err != nil {
+		t.Error(err)
+	} else if output, err := Client.SearchMember(userID); err != nil {
+		t.Error(err)
+	} else if output.MemberID != userID || output.MemberName != "Miss Tester" {
+		t.Errorf("User should be updated successfully")
 	}
 
-	if _, err := Client.DeleteMember(userID); checkErr(err, t) == nil {
-		if _, err := Client.SearchMember(userID); err == nil {
-			t.Errorf("User should be deleted successfully")
-		}
+	if _, err := Client.DeleteMember(userID); err != nil {
+		t.Error(err)
+	} else if _, err := Client.SearchMember(userID); err == nil {
+		t.Errorf("User should be deleted successfully")
 	}
 }
 
 func TestCardAPIs(t *testing.T) {
 	userID := fmt.Sprintf("%v", time.Now().UnixNano())
-	Client.RegisterMember(userID, "jinzhu")
+	Client.RegisterMember(userID, "Mr Tester")
 
-	if output, err := Client.SaveCard(userID, "4024007154567043", "1219", "jinzhu"); checkErr(err, t) == nil {
-		seq := output.CardSeq
-		if _, err := Client.SearchCard(userID, seq); checkErr(err, t) != nil {
-			t.Errorf("Card should be created successfully")
-		}
+	output, err := Client.SaveCard(userID, "4024007154567043", "1010", "Mr Tester")
+	if err != nil {
+		t.Error(err)
+	}
+	seq := output.CardSeq
+	if _, err := Client.SearchCard(userID, seq); err != nil {
+		t.Error(err)
+	}
 
-		if _, err := Client.SaveCard(userID, "4024007154567043", "1219", "jinzhu new"); checkErr(err, t) == nil {
-			if output, err := Client.SearchCard(userID, seq); checkErr(err, t) != nil || output.HolderName != "jinzhu new" {
-				t.Errorf("Card should be updated successfully")
-			}
-		}
+	if _, err := Client.SaveCard(userID, "4024007154567043", "1010", "Miss Tester"); err != nil {
+		t.Error(err)
+	}
+	if output, err := Client.SearchCard(userID, seq); err != nil {
+		t.Error(err)
+	} else if output.HolderName != "Miss Tester" {
+		t.Error("HolderName = %s; want Miss Tester", output.HolderName)
+	}
 
-		if _, err := Client.DeleteCard(userID, seq); checkErr(err, t) == nil {
-			if output, err := Client.SearchCard(userID, seq); checkErr(err, t) != nil || output.DeleteFlag != "1" {
-				t.Errorf("Card should be deleted successfully")
-			}
-		}
+	if _, err := Client.DeleteCard(userID, seq); err != nil {
+		t.Error(err)
+	}
+	if output, err := Client.SearchCard(userID, seq); err != nil {
+		t.Error(err)
+	} else if output.DeleteFlag != "1" {
+		t.Errorf("DeleteFlag = %s; want 1", output.DeleteFlag)
 	}
 }
 
-func TestCreateOrderWithSavedCard(t *testing.T) {
+func TestCreditCardOrderAPIs(t *testing.T) {
 	userID := fmt.Sprintf("%v", time.Now().UnixNano())
-	Client.RegisterMember(userID, "jinzhu")
-	Client.SaveCard(userID, "4111111111111111", "1219", "jinzhu")
-	orderID := userID
+	if _, err := Client.RegisterMember(userID, "Mr Tetser"); err != nil {
+		t.Fatal(err)
+	}
+	savedCard, err := Client.SaveCard(userID, "4111111111111111", "0101", "Mr Tetser")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	if output, err := Client.EntryTran(orderID, "1000", "100"); checkErr(err, t) == nil {
-		if output, err := Client.ExecTran(output.AccessID, output.AccessPass, orderID, userID, "0", "123"); checkErr(err, t) != nil || output.Approve == "" {
-			t.Error("Should charge order with registered card")
-		}
-	} else {
-		t.Errorf("No error should happen when register order to GMO")
+	orderID := fmt.Sprintf("%v", time.Now().UnixNano())
+	t.Log("Order ID:", orderID)
+	entryOutput, err := Client.EntryTran(orderID, "1000", "100")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	execOutput, err := Client.ExecTran(entryOutput.AccessID, entryOutput.AccessPass, orderID, userID, savedCard.CardSeq, "0101")
+	if err != nil {
+		t.Error(err)
+	} else if execOutput.Approve == "" {
+		t.Error("Approve should not be empty")
+	}
+
+	if _, err := Client.ChangeTran(entryOutput.AccessID, entryOutput.AccessPass, "1500", "100"); err != nil {
+		t.Fatal(err)
+	}
+	if searchOutput, err := Client.SearchTrade(orderID); err != nil {
+		t.Error(err)
+	} else if searchOutput.Amount != "1500" {
+		t.Error("Amount = %s; want 1500", searchOutput.Amount)
+	}
+
+	if _, err := Client.CancelTran(entryOutput.AccessID, entryOutput.AccessPass); err != nil {
+		t.Fatal(err)
+	}
+	if searchOutput, err := Client.SearchTrade(orderID); err != nil {
+		t.Error(err)
+	} else if searchOutput.Status != "VOID" {
+		t.Errorf("Status = %s; want VOID", searchOutput.Status)
 	}
 }
 
-func TestCreatePaypalOrder(t *testing.T) {
+func TestPaypalOrderAPIs(t *testing.T) {
 	orderID := fmt.Sprintf("%v", time.Now().UnixNano())
 
-	if output, err := Client.EntryTranPaypal(orderID, "1000", "100", "USD"); checkErr(err, t) == nil {
-		if _, err := Client.ExecTranPaypal(output.AccessID, output.AccessPass, orderID, "Test Order", "http://theplant.jp/gmo_redirect"); checkErr(err, t) != nil {
-			t.Error("Should charge order with registered card")
-		}
-	} else {
-		t.Errorf("No error should happen when register paypal order to GMO")
+	entryOutput, err := Client.EntryTranPaypal(orderID, "1000", "100", "JPY")
+	if err != nil {
+		t.Error(err)
+	}
+	if _, err := Client.ExecTranPaypal(entryOutput.AccessID, entryOutput.AccessPass, orderID, "Test Order", "http://theplant.jp/gmo_redirect"); err != nil {
+		t.Error(err)
 	}
 }
